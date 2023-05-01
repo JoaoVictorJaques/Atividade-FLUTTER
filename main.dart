acc4feb6cd0211f218c2a6c43bdbc001bde55f6f
João Victor Jaques <130093038+JoaoVictorJaques@users.noreply.github.com>
2023-05-01 12:33:03 -0300

    atividade PDM
    Atividade componentes de estado

--- main.dart
+++ main.dart
@@ -1,51 +1,67 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var contador = 0;
+
+  final perguntas = [
+    "Sua cor favorita?",
+    "Seu animal favorito?",
+    "Sua rede social favorita?",
+    "Sua comida preferida?"
+  ];
+
+  void clicou() {
+    setState(() {
+      contador++;
+    });
+    print(contador);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My App'),
+          title: const Text('Meu primeiro APP!'),
         ),
-        body: Row(
+        body: Column(
           children: [
-            Expanded(
-              child: Column(
-                children: const [
-                  Text('Aprendendo'),
-                  Text('Programação'),
-                  Text('Flutter'),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Enviar'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Cancelar'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Salvar'),
-                  ),
-                ],
-              ),
-            ),
+            Text(perguntas[contador]),
+            ElevatedButton(onPressed: clicou, child: const Text('Clique aqui')),
+            ElevatedButton(
+                onPressed: () {
+                  print("outra função");
+                },
+                child: const Text('Clique aqui')),
+            ElevatedButton(
+                onPressed: () {
+                  print("outra função");
+                },
+                child: const Text('Clique aqui')),
+            ElevatedButton(
+                onPressed: () => print("Função arrow"),
+                child: const Text('Clique aqui')),
           ],
         ),
       ),
     );
+  }
+
+  // ignore: no_logic_in_create_state
+  State<StatefulWidget> createState() {
+    throw UnimplementedError();
   }
 }
